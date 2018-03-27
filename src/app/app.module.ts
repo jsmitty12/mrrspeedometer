328f168e7043e930ef9cb90f8b343dd4c8eefc9c
Joshua Smith <jsmith@jssm.com>
2018-03-27 15:50:35 -0500

    Add BrowserAnimationModule, FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 
@@ -7,19 +9,24 @@ import {ServiceWorkerModule} from '@angular/service-worker';
 import {AppComponent} from './app.component';
 
 import {environment} from '../environments/environment';
-import { MainViewComponent } from './views/main-view/main-view.component';
+import {MainViewComponent} from './views/main-view/main-view.component';
+import {CardModule, DropdownModule} from "primeng/primeng";
 
 @NgModule({
     declarations: [
         AppComponent,
-        MainViewComponent
+        MainViewComponent,
     ],
     entryComponents: [
         MainViewComponent
     ],
     imports: [
-        BrowserModule,
         AppRoutingModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        CardModule,
+        DropdownModule,
+        FormsModule,
         ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
     ],
     providers: [],
